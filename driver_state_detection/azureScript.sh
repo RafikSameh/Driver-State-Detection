3aefd113e13aaeeaedff32bf8e9100177bd339fe
rafiksameh <rafiksameh123@gmail.com>
2025-05-18 21:00:20 +0000

    azure logs retrieving script

---
+++ driver_state_detection/azureScript.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+azcopy copy "../distraction_log.txt" "https://rafiksameh.blob.core.windows.net/logs?sp=racwdli&st=2025-05-18T20:55:33Z&se=2025-07-11T04:55:33Z&sv=2024-11-04&sr=c&sig=y6GUiwYbgpg6fgLg7gOpc%2F5vEJugaq%2B1XTSHx%2F4QoTE%3D"
